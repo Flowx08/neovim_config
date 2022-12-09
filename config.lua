cb7e0593df55f38eb06f41b8246ccae520e76aa4
System Administrator <root@Carlos-MacBook-Air.local>
2022-12-09 12:01:29 +0100

    Update

--- config.lua
+++ config.lua
@@ -142,10 +142,6 @@ cmp.setup.cmdline(':', {
 
 -- Set up lspconfig.
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
-	capabilities = capabilities
-}
 
 require('lspconfig').clangd.setup{}
 
